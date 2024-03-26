b832cf3274ec32a09a821323d12f5c23913a84b0
Plyampus <plyampus@gmail.com>
2024-03-26 13:53:22 +0200

    Update Mentor's Request

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -22,6 +22,8 @@ const options = {
         position: 'topRight',
         message: `Please choose a date in the future`,
       });
+
+      inputTime.disabled = false;
     } else {
       startBtn.disabled = false;
       inputTime.disabled = true;
@@ -30,21 +32,14 @@ const options = {
 };
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
-
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
   return { days, hours, minutes, seconds };
 }
 
@@ -53,25 +48,18 @@ const inputTime = document.querySelector('#datetime-picker');
 const startBtn = document.querySelector('[data-start]');
 const timeValueElements = document.querySelectorAll('.value');
 
-
-console.log(timeValueElements);
-
 startBtn.disabled = true;
-
 startBtn.addEventListener('click', event => {
   const repeatTime = setInterval(() => {
     timeInterval = userSelectedDate - new Date();
-    inputTime.disabled = true;
-
+    
     if (timeInterval < 0) {
       startBtn.disabled = true;
       inputTime.disabled = false;
       clearInterval(repeatTime);
       return;
     }
-
     const timer = convertMs(timeInterval);
-
     timeValueElements[0].textContent = timer.days.toString().padStart(2, '0');
     timeValueElements[1].textContent = timer.hours.toString().padStart(2, '0');
     timeValueElements[2].textContent = timer.minutes.toString().padStart(2, '0');
--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -2,36 +2,36 @@ import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
 const formOfDelay = document.querySelector(".form");
-
 formOfDelay.addEventListener('submit', event => {
-  event.preventDefault();
-  
-  const timer = parseInt(event.currentTarget.elements.delay.value);
-  const radio = event.currentTarget.elements.state.value;
-  
-  const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (radio === "fulfilled") {
-        resolve(timer);
-      } else {
-        reject(timer);
-      }
-    }, timer);
-  });
+    event.preventDefault();
+    const delay = parseInt(event.currentTarget.elements.delay.value);
 
-  promise
-    .then(delayValue => {
-      iziToast.success({
-        color: 'green',
-        position: "topRight",
-        message: `Fulfilled promise in ${delayValue}ms`
-      });
-    })
-    .catch(delayValue => {
-      iziToast.error({
-        color: 'red',
-        position: "topRight",
-        message: `Rejected promise in ${delayValue}ms`
-      });
+    const selectedRadioButton = event.currentTarget.querySelector('input[name="state"]:checked');
+    const promiseState = selectedRadioButton ? selectedRadioButton.value : null;
+
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (promiseState === "fulfilled") {
+                resolve(delay);
+            } else {
+                reject(delay);
+            }
+        }, delay);
+    });
+
+    promise.then((delayValue) => {
+        iziToast.success({
+            title: 'Success',
+            color: 'green',
+            position: "topRight",
+            message: `Fulfilled promise in ${delayValue}ms`,
+        });
+    }).catch((delayValue) => {
+        iziToast.error({
+            title: 'Error',
+            color: 'red',
+            position: "topRight",
+            message: `Rejected promise in ${delayValue}ms`,
+        });
     });
 });
